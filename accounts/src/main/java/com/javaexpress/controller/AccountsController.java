d2c809af8aed8c3752832fc720df5ccb55158e7c
javaexpresschannel <javaexpresschannel@gmail.com>
2023-09-17 21:55:28 +0530

    second commit

--- accounts/src/main/java/com/javaexpress/controller/AccountsController.java
+++ accounts/src/main/java/com/javaexpress/controller/AccountsController.java
@@ -1,6 +1,5 @@
 package com.javaexpress.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.javaexpress.model.Account;
 import com.javaexpress.service.AccountsService;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
 @RequestMapping("/api/v1/accounts")
+@AllArgsConstructor
 public class AccountsController {
 	
-	@Autowired
 	private AccountsService accountsService;
 	
 	@GetMapping("/{customerId}")
--- loans/src/main/java/com/javaexpress/loans/models/Loans.java
+++ loans/src/main/java/com/javaexpress/loans/models/Loans.java
@@ -2,11 +2,10 @@ package com.javaexpress.loans.models;
 
 import java.sql.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
